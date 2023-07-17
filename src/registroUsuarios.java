b1c7d3627dee102c91813e996d13cc31eb512fa3
edusebass <eduardoalmachi123@hotmail.com>
2023-07-17 10:42:52 -0500

    creacion del form para registro

---
+++ src/registroUsuarios.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class registroUsuarios {
+    private JTextField textField1;
+    private JButton registrarButton;
+    private JButton iniciarSesionButton;
+    private JPasswordField passwordField1;
+}
